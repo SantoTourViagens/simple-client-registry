6607a048ae01612884cb7b76fbc77a400f74a407
appsantotour <santotourviagens@gmail.com>
2025-03-28 23:37:56 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
       '1c77c54c-8e3d-42bd-acb2-dc81cad06f8b.lovableproject.com',
       '117003f8-1b25-4efb-bfbd-ffe73ad40823.lovableproject.com',
       '8ebcacba-c862-44e2-9a1c-388f748ee1a4.lovableproject.com'
+      '106bbda2-5978-487d-bf4a-3d393ef3a915.lovableproject.com'
     ],
     mimeTypes: {
       'text/javascript': ['ts', 'tsx', 'js', 'jsx'],
